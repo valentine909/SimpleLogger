d3615f6e572a7299cd07cc1373407aefe9de62fc
Valentine Nazarov <valentine.nazarov@gmail.com>
2019-12-27 12:23:24 +0300

    simplified

--- logger.py
+++ logger.py
@@ -1,16 +1,9 @@
 from time import sleep
-from datetime import date, datetime
-DELAY = 0.01  # In minutes
+from datetime import datetime
+DELAY = 1  # In minutes
 
-current_date = str(date.today())
-c = 0
-while c < 5:
-    if str(date.today()) == current_date:
-        with open(current_date + ".txt", "a+") as f:
-            f.write(str(datetime.now().strftime("%d/%m/%Y %H:%M:%S")) + "\n")
-    else:
-        current_date = str(date.today())
-        with open(current_date + ".txt", "w") as f:
-            f.write(str(datetime.now().strftime("%d/%m/%Y %H:%M:%S")) + "\n")
+while True:
+    with open("log.txt", "w") as f:
+        print(str(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+        f.write(str(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
     sleep(60 * DELAY)
-    c += 1
